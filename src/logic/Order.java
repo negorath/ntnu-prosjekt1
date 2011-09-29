18d4ef57f85a91cc04aa679a43f12a9af03a7d83
Christian Tellnes <christian@tellnes.no>
2011-09-29 17:03:43 +0200

    create Order api

--- src/logic/Order.java
+++ src/logic/Order.java
@@ -1,1 +1,42 @@
 package logic;
+
+class Order {
+	
+	private int id;
+	private int userId;
+	private Product[] products;
+	
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the products
+	 */
+	public Product[] getProducts() {
+		return products;
+	}
+	/**
+	 * @param products the products to set
+	 */
+	public void setProducts(Product[] products) {
+		this.products = products;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	
+	
+}
