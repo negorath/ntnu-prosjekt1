28f009f9c5482ff89631b1de7d2ac6987d1ce3a4
Christian Tellnes <christian@tellnes.no>
2011-09-29 17:17:54 +0200

    add comments to ConfigSample

---
+++ src/ConfigSample.java
@@ -0,0 +1,16 @@
+/*
+ * Koppier denne filen til Config.java og endre navn på den.
+ * Hvis sample filen endres, må du oppdatere config filen din.
+ * 
+ * Her kan du fylle inn påloggingsinformasjonen til MySQL
+ */
+
+
+public class ConfigSample {
+
+	static String DBHost = "localhost";
+	static String DBUsername = "prosjekt1";
+	static String DBPassword = "";
+	static String DBName = "prosjekt1";
+	
+}
