2903f28f2d7c3929957924f9ca85e27581a3dbf5
Alf Mikael Constantinou <amikaelc@gmail.com>
2011-10-19 23:38:21 +0200

    Lagt til en klasse for å koble sammen programmet til databasen. Flyttet ConfigSample til en ny pakke: localData

---
+++ src/localData/ConfigSample.java
@@ -0,0 +1,17 @@
+package localData;
+
+/*
+ * Koppier denne filen til Config.java og endre navn på den.
+ * Hvis sample filen endres, må du oppdatere config filen din.
+ * 
+ * Her kan du fylle inn påloggingsinformasjonen til MySQL
+ */
+public class ConfigSample {
+
+	public static String DBHost = "//localhost";
+	public static String DBUsername = "prosjekt1";
+	public static String DBport = "3306";
+	public static String DBPassword = "";
+	public static String DBName = "prosjekt1";
+	
+}
