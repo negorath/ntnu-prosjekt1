79bf0e3f6f7351db17563a6e3ab79792b6f99f46
Alf Mikael Constantinou <amikaelc@gmail.com>
2011-10-27 17:49:46 +0200

    Endret navn paa 'Version' til det mer passende DatabaseConnector

---
+++ src/logic/DatabaseConnector.java
@@ -0,0 +1,56 @@
+package logic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import localData.ConfigSample;
+
+public class DatabaseConnector {
+
+    public static void main(String[] args) {
+
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        
+        String url = "jdbc:mysql:" + ConfigSample.DBHost + ":" + ConfigSample.DBport + "/" + ConfigSample.DBName;
+        String user = ConfigSample.DBUsername;
+        String password = ConfigSample.DBPassword;
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(Version.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(Version.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
+}
