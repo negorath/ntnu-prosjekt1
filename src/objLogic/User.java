33807e380438b5b04f8d567efe07fd5b9ed34cba
Alf Mikael Constantinou <amikaelc@gmail.com>
2011-11-10 22:34:18 +0100

    implementert retrieve og sync funksjonene

--- src/objLogic/User.java
+++ src/objLogic/User.java
@@ -7,7 +7,6 @@ import logic.Address;
 
 
 
-import com.mysql.jdbc.ResultSet;
 
 
 public class User{
@@ -16,34 +15,45 @@ public class User{
 	private String phone;
 	private Address address;
 	private java.sql.Connection con;
+	
+	
 	public User(String name, String phone, Address address){
 		this.name = name;
 		this.phone = phone;
 		this.address = address;
 	}
-	public void add(User user) throws SQLException{
-		
-		    	
-		
-	}
+	
 	
 	//Kode tatt fra DatabaseConnector, her er det den skal være
 	public User retrieve(String phoneNumber) throws Exception{
 		Statement stmt = con.createStatement();
 		
-    	ResultSet getUser_rs = stmt.executeQuery("SELECT name, phone, address_id FROM users WHERE phone='" + phoneNumber + "'");
-    	getUser_rs.first();
-    	String name = getUser_rs.getString(1);
-    	String phone = getUser_rs.getString(2);
-    	String address_id = getUser_rs.getString(3);
-    	getUser_rs.close();
+    	ResultSet getUser_rs = stmt.executeQuery("SELECT * FROM users WHERE phone='" + phoneNumber + "'");
+    	if (getUser_rs.next()) { //if it is not empty
+    		String name = getUser_rs.getString("name");
+        	String phone = getUser_rs.getString("phone");
+        	String address_id = getUser_rs.getString("address_id");
+        	getUser_rs.close();
+        	
+        	
+        	ResultSet adrData = stmt.executeQuery("SELECT * FROM addresses WHERE id='" + address_id + "'");
+        	Address adr; //address
+        	if (adrData.first()) {
+        		adr = new Address();
+        		adr.setCountry(adrData.getString("country"));
+        		adr.setCity(adrData.getString("city"));
+        		adr.setZipcode(adrData.getString("zipcode"));
+        		adr.setStreet(adrData.getString("street"));
+        		adr.setHouseNumber(adrData.getInt("houseNumber"));
+        		adr.setHouseLetter(adrData.getString("houseLetter"));
+        	}
+        	else {
+        		address = null;
+        	}
+        	return new User(name, phone, address);
+        	
+    	} else return null;
     	
-    	ResultSet getAddress_rs = stmt.executeQuery("SELECT street, houseNumber, zipcode, city FROM addresses WHERE id='"+address_id+"'");
-    	getAddress_rs.first();
-    	Address address = new Address(getAddress_rs.getString(1), Integer.parseInt(getAddress_rs.getString(2)), getAddress_rs.getString(3), getAddress_rs.getString(4));
-    	getAddress_rs.close();
-    	return new User(name, phone, address);
-		
 		
 	}
 //	public User retrive(int phoneNumber) throws Exception{
@@ -61,11 +71,10 @@ public class User{
 
 	/**
 	 * @param name the name to set
+	 * @throws SQLException 
 	 */
-	public void setName(String name) {
+	public void setName(String name) throws SQLException {
 		this.name = name;
-		
-		
 	}
 
 	/**
@@ -77,9 +86,10 @@ public class User{
 
 	/**
 	 * @param phone the phone to set
+	 * @throws SQLException 
 	 */
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public void setPhone(String phone) throws SQLException {
+		
 	}
 
 	/**
@@ -91,14 +101,42 @@ public class User{
 
 	/**
 	 * @param address the address to set
+	 * @throws SQLException 
 	 */
-	public void setAddress(Address address) {
-		this.address = address;
+	public void setAddress(Address address) throws SQLException {
+		
+		
 	}
 	
 	public String toString(){
 		String result;
 		result = getPhone() + ": " + getName();
 		return result;
+	}
+	/**Synkroniserer dette objektet med databasen. Hvis det ikke eksisterer fra før, så opprettes det. Hvis det eksisterer fra før så blir alle feltene overskrevet.
+	 * 
+	 * 
+	 * 
+	 * @throws SQLException
+	 */
+	public void sync() throws SQLException{
+		
+		Statement stmt = con.createStatement();
+		boolean existsInDatabase = stmt.execute("SELECT phone WHERE phone='" + this.getPhone() + "'");
+		
+		//If this person already exist, just update the specific record
+		if (!existsInDatabase) {
+			
+			stmt.execute(	"UPDATE users" +
+							"SET address_id='" + address.getId() +"', name='" + this.getName() + "', " +
+							"WHERE phone='" + this.getPhone());
+		}
+		else { //If this person does not exist, create them
+			stmt.execute(	"INSERT INTO users VALUES (" + 
+							this.getName() + ", " + 
+							this.getPhone() + ", " + 
+							this.getAddress().getId() + ")");
+		}
+		address.sync();
 	}
 }
