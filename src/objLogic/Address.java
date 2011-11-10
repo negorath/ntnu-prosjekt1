5a3ee89825865eb13e1db7c5cc47728ad9d7b19b
Alf Mikael Constantinou <amikaelc@gmail.com>
2011-11-10 23:59:22 +0100

    Implementert database-funksjonalitet i address klassen

--- src/objLogic/Address.java
+++ src/objLogic/Address.java
@@ -1,7 +1,17 @@
 package objLogic;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Address {
+	private int id;
+	private String street;
+	private int houseNumber;
+	private String houseLetter;
+	private String zipcode;
+	private String city;
+	private String country;
 	
 	public static Connection con;
 	/**Connect to the given database Connection
@@ -10,6 +20,9 @@ public class Address {
 	 */
 	public static void setConnection(Connection c) {
 		con = c;
+	}
+	public static Connection getConnection() {
+		return con;
 	}
 	
 	/**
@@ -25,15 +38,17 @@ public class Address {
 		this.houseNumber = houseNumber;
 		this.zipcode = zipcode;
 		this.city = city;
+		this.id = -1;
 	}
+	public Address() {
+		this.street = null;
+		this.houseNumber = -1;
+		this.zipcode = null;
+		this.city = null;
+		this.id = -1;
+	}
+	
 
-	private int id;
-	private String street;
-	private int houseNumber;
-	private char houseLetter;
-	private String zipcode;
-	private String city;
-	private String country;
 	/**
 	 * @return the address
 	 */
@@ -49,6 +64,7 @@ public class Address {
 	/**
 	 * @return the houseNumber
 	 */
+	
 	public int getHouseNumber() {
 		return houseNumber;
 	}
@@ -61,13 +77,13 @@ public class Address {
 	/**
 	 * @return the houseLetter
 	 */
-	public char getHouseLetter() {
+	public String getHouseLetter() {
 		return houseLetter;
 	}
 	/**
 	 * @param houseLetter the houseLetter to set
 	 */
-	public void setHouseLetter(char houseLetter) {
+	public void setHouseLetter(String houseLetter) {
 		this.houseLetter = houseLetter;
 	}
 	/**
@@ -116,4 +132,94 @@ public class Address {
 		this.id = id;
 	}
 	
+	/** Returns the address of the given valid id
+	 * 
+	 * @return Address from database
+	 */
+	public static Address retrieve(String id) throws SQLException{
+		
+		Statement stmt = con.createStatement();
+		
+		ResultSet data = stmt.executeQuery(	"SELECT * FROM addresses " +
+											"WHERE id='" + id + "'");
+		
+		if (data.first()) {
+			Address address = new Address();
+			address.setStreet(data.getString("street"));
+			address.setHouseNumber(data.getInt("houseNumber"));
+			address.setHouseLetter(data.getString("houseLetter"));
+			address.setZipcode(data.getString("zipcode"));
+			address.setCity(data.getString("city"));
+			address.setCountry(data.getString("country"));
+			return address;
+		}
+		else return null;
+		
+		
+	}
+		
+//	/**Takes an address as input, checks if it can find an exact copy of it within the database
+//	 * 
+//	 * @param address
+//	 * @return The same object with the proper addressId, null if it does not exist
+//	 * @throws SQLException 
+//	 */
+//	public static Address retrieve(Address adr) throws SQLException {
+//		Statement stmt = con.createStatement();
+//		
+//		boolean existsInDatabase = stmt.execute("SELECT * FROM addresses" +
+//												"WHERE street='" + adr.getStreet() + "' " +
+//												"AND houseNumber='" + adr.getHouseNumber() + "' " +
+//												"AND houseLetter='" + adr.getHouseLetter() + "' " +
+//												"AND zipcode='" + adr.getZipcode() + "' " +
+//												"AND city='" + adr.getCity() + "' " +
+//												"AND country='" + adr.getCountry() + "' ");
+//		
+//		if (existsInDatabase) {
+//			String addressId = stmt.getResultSet().getString("id");
+//			adr.setId(Integer.parseInt(addressId));
+//			return adr;
+//		}
+//		return null;
+//		
+//	}
+	/**Synkroniserer dette objektet med databasen. Hvis det ikke eksisterer fra før, så opprettes det. Hvis det eksisterer fra før så blir alle feltene overskrevet.
+	 * 
+	 * 
+	 * 
+	 * @throws SQLException
+	 */
+	public void sync() throws SQLException{
+		
+		Statement stmt = con.createStatement();
+//		If this object already exists in the database, update the values 
+		if (this.getId() != -1) {
+			stmt.execute(	"UPDATE addresses " +
+							"SET country='" + this.getCountry() + 
+							"', city='" + this.getCity() + 
+							"', zipcode='" + this.getZipcode() + 
+							"', street='" + this.getStreet() + 
+							"', houseNumber='" + this.getHouseNumber() +
+							"', houseLetter='" + this.getHouseLetter());
+		}
+		else 	{  // Create the address
+			
+			stmt.execute(	"INSERT INTO addresses VALUES (" + 
+							this.getStreet() + ", " +
+							this.getHouseNumber() + ", " + 
+							this.getHouseLetter() + ", " + 
+							this.getZipcode() + ", " +
+							this.getCity() + ", " + 
+							this.getCountry() + ", )" );
+		}
+	
+	}
+	
+	public void delete() throws SQLException{
+		this.sync();
+		Statement stmt = con.createStatement();
+		
+		stmt.execute(	"DELETE from addresses " +
+						"WHERE id='" + this.getId() + "'");
+	}
 }
