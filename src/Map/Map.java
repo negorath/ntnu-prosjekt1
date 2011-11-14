84e31597abada9c1491dc02d8a8e4c3f1cdea9d5
Robin Tordly <robin.tordly@gmail.com>
2011-11-14 17:25:25 +0100

    rydder opp i source folder for aa lage class-diagram

---
+++ src/Map/Map.java
@@ -0,0 +1,26 @@
+package Map;
+
+import java.awt.Component;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Map {
+	public void call(String s, String tittle) throws Exception{
+	    JFrame frame = new JFrame();
+	    JPanel panel = new JPanel();
+	    BufferedImage image = ImageIO.read(new URL(s));
+	    JLabel label = new JLabel(new ImageIcon(image));
+	    panel.add(label);
+	    frame.add(panel);
+	    frame.setTitle(tittle);
+	    frame.pack();
+	    frame.setLocationRelativeTo(null);
+	    frame.setVisible(true);
+	}
+}
--- src/objLogic/HelpUser.java
+++ src/objLogic/HelpUser.java
@@ -1,16 +1,16 @@
-package objLogic;
-
-
-public class HelpUser {
-
-	public static User getBob() {
-		User bob = new User();
-		bob.setName("bob");
-		bob.setPhone("81549300");
-		bob.setAddress(new Address("Nardoveien", 5, "7046", "Trondheim"));
-		return bob;
-		
-	}
-	
-	
-}
+//package objLogic;
+//
+//
+//public class HelpUser {
+//
+//	public static User getBob() {
+//		User bob = new User();
+//		bob.setName("bob");
+//		bob.setPhone("81549300");
+//		bob.setAddress(new Address("Nardoveien", 5, "7046", "Trondheim"));
+//		return bob;
+//		
+//	}
+//	
+//	
+//}
